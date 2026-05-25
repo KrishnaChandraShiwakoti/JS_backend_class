eb3076ca91b65ab3f4d4e2b397de25dd453622df
Rijan Shiwakoti <rijankrishna14@gmail.com>
2026-05-25 09:25:48 +0545

    Add MONGODB_URI export to configuration constants

--- src/config/constant.ts
+++ src/config/constant.ts
@@ -3,6 +3,7 @@ dotenv.config();
 
 export const PORT = process.env.PORT || 8088;
 export const MOCK = process.env.MOCK_DB || "mock";
+export const MONGODB_URI = process.env.MONGODB_URI;
 
 //same as
 //export{
